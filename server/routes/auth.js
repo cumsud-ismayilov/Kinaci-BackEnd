a7493b1985f645cf57b4115be8f91e8cc15ff340
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-11-06 15:16:39 +0400

    all changes add repo

---
+++ server/routes/auth.js
@@ -0,0 +1,24 @@
+import express from "express";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const exist = await User.findOne({ email });
+  if (exist) return res.status(400).json({ message: "Email artıq mövcuddur" });
+  await User.create({ name, email, password });
+  res.json({ message: "Qeydiyyat uğurlu ✅" });
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "İstifadəçi tapılmadı ❌" });
+  if (user.password !== password) return res.status(400).json({ message: "Şifrə yalnışdır ❌" });
+  res.json({ message: "Daxil olundu ✅", user });
+});
+
+export default router;
