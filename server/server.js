83a0455ce182911d32903d504c499616e0207b2d
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-11-19 19:45:00 +0400

    add all fiel

--- server/server.js
+++ server/server.js
@@ -5,30 +5,37 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import contactRoutes from "./routes/contact.js";
 import commentsRouter from "./routes/comment.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// ✅ .env faylını yüklə
+dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// ✅ API routeları
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/comments", commentsRouter);
 
-
 // ✅ MongoDB-ə qoşulma
 mongoose
-  .connect(
-    "mongodb+srv://csmayilov1_db_user:rHsIbn3MDaGSKxvl@kinacicluster.s5vgty3.mongodb.net/?appName=KinaciCluster"
-  )
+  .connect(process.env.DB_URL)
   .then(() => console.log("✅ MongoDB Atlas-a uğurla qoşuldu"))
   .catch((err) => console.log("❌ Bağlantı xətası:", err));
 
-app.listen(5000, () => {
-  console.log("Server işə düşdü → http://localhost:5000");
-});
+// ✅ Server listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server işə düşdü → http://localhost:${PORT}`));
 
 app.get("/", (req, res) => {
   res.send("API işləyir 🚀");
 });
+
 
 // csmayilov1_db_user
 // rHsIbn3MDaGSKxvl
