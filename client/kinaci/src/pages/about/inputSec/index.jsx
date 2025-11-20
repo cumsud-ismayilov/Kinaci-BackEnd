dad187e429652c828c5f39f895d917ad60f1e8b2
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-11-20 18:00:29 +0400

    contact page api url add

--- client/kinaci/src/pages/about/inputSec/index.jsx
+++ client/kinaci/src/pages/about/inputSec/index.jsx
@@ -25,6 +25,8 @@ function InputSec() {
     }
   }, []);
 
+  const API_URL = import.meta.env.VITE_API_URL;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -37,7 +39,7 @@ function InputSec() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch(`${API_URL}/api/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...formData, userId: user._id }),
